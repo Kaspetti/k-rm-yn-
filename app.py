79eaea8037b09c480a7d5d520a2bba6d9c1cb7d6
Kaspar Moberg <kaspar@kasparm.home>
2024-06-08 23:38:53 +0200

    added image request in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, send_file
 import pandas as pd
+import os
 
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ def index():
 
 
 @app.route("/api/data")
-def data():
+def get_data():
     df = pd.read_csv("./static/data/KARMY-_punkter.csv")
     df['WKT'] = df['WKT'].str[7:-2].str.split(' ')
 
@@ -19,3 +20,15 @@ def data():
             df.to_json(orient="records"),
             mimetype="application/json"
         )
+
+
+@app.route("/api/images")
+def get_image():
+    image_id = request.args.get("id")
+    if not image_id:
+        return send_file("./static/images/placeholder.jpg")
+
+    if not any(f.split(".")[0] == image_id for f in os.listdir("./static/images/")):
+        return send_file("./static/images/placeholder.jpg")
+
+    return send_file(f"./static/images/{image_id}.jpg", mimetype="image/jpeg")
